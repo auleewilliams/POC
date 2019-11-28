730b8bc208ce4abf0c73d19cf6e118e51a810ac4
Lee Williams <lee.williams@mater.org.au>
2019-11-28 10:17:29 +1000

    POC for Stripe integration.

---
+++ Stripe-API-Integration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Stripe;
+
+namespace Stripe_API_Integration
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var apiKey = "";
+            var stripe = new StripeClient(apiKey);
+
+            var chargeService = new ChargeService(stripe);
+            var chargeId = "";
+            var charge = chargeService.Get(chargeId);
+
+            Console.WriteLine("Charge");
+            Console.WriteLine($"Amount: ${charge.Amount}");
+
+            var payoutService = new PayoutService(stripe);
+            var payoutId = "";
+            var payout = payoutService.Get(payoutId);
+
+            Console.WriteLine($"Payout: {payoutId}");
+            Console.WriteLine($"Date Paid: {payout.ArrivalDate.ToString()}");
+            
+
+            var balanceTransactionService = new BalanceTransactionService(stripe);
+            var requestOptions = new BalanceTransactionListOptions {Payout = payoutId, Limit = 100};
+            var transactionList = balanceTransactionService.List(requestOptions);
+
+            Console.WriteLine($"Transactions: {transactionList.Count()}");
+
+        }
+    }
+}
